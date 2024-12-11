083630a45efd611eda6f287452707420f4ff9b9d
LILAD <lidetadmassu217@gmail.com>
2024-12-11 14:01:18 +0300

    user authentication

---
+++ user authentication/user.py
@@ -0,0 +1,9 @@
+import hashlib
+class User:
+    def __init__(self,username,password):
+        self.userName= username
+        self.password_hash= self.hash_password(password)
+    def hash_password(self,password):
+        return hashlib.sha256(password.encode()).hexdigest()
+    def check_password(self,password):
+        return self.password_hash==self.hash_password(password)
